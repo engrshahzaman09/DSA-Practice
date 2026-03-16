8fcc5fff3d4da14a37909a0fe114a82f092fe42e
Shah Zaman <engrshahzaman09@gmail.com>
2026-03-17 03:53:48 +0500

    Day 1 for DSA practice

---
+++ src/Day2_Strings7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Day2_Strings7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Word : ");
+
+        String name = sc.nextLine();
+
+        System.out.println(name.toLowerCase());
+    }
+}
