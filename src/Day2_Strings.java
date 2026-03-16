be532cc331ca947ae255d70622806db4ecf62b63
Shah Zaman <engrshahzaman09@gmail.com>
2026-03-17 02:06:19 +0500

    Day 1 for DSA practice

---
+++ src/Day2_Strings.java
@@ -0,0 +1,8 @@
+public class Day2_Strings {
+    public static void main(String[] args) {
+
+        String name = "Shah Zaman";
+        System.out.println("Name length is : "+ name.length());
+    }
+
+}
